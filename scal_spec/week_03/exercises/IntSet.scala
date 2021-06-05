2be24468f27d7c92f522cf89542823de16f206b1
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-06-05 16:56:16 -0400

    IntSet exercise

---
+++ scal_spec/week_03/exercises/IntSet.scala
@@ -0,0 +1,37 @@
+#!/bin/env /bin/scala3
+/**
+ * A few definitions for working with Integer sets
+ */
+
+object IntSet_exercise{
+  abstract class IntSet:
+    def incl(x: Int): IntSet
+    def contains(x: Int): Boolean
+    def union(other: IntSet): IntSet
+  
+  object Empty extends IntSet:
+    def contains(x: Int): Boolean = false
+    def incl(x: Int): IntSet = NonEmpty(x, Empty, Empty)
+    def union(other: IntSet): IntSet = other
+  end Empty
+
+  class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet: 
+    def contains(x: Int): Boolean = 
+      if (x < elem) left.contains(x)
+      else if (x > elem) right.contains(x)
+      else true
+    def incl(x: Int): IntSet = 
+      if (x < elem) NonEmpty(elem, left.incl(x), right)
+      else if (x > elem) NonEmpty(elem, left, right.incl(x))
+      else this
+    def union(other: IntSet): IntSet =
+      left.union(right).union(other).incl(elem)
+  end NonEmpty
+
+  
+  def main(args: Array[String]): Unit = {
+    println("hello world!")
+
+    }
+}
+
