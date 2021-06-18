b6b0efe3f61cf988d59bcb52ba7f37e981a9f7b2
Joe Mifsud <45021583+4d30@users.noreply.github.com>
2021-06-18 15:23:15 -0400

    lecture exercise

--- scal_spec/week_04/exercises/decomp.scala
+++ scal_spec/week_04/exercises/decomp.scala
@@ -6,32 +6,33 @@
 object pme{
   
   trait Expr
-  case class Numberr(n: Int) extends Expr
-  case class Sum(e1: Expr, e2: Expr) extends Expr
-  case class Var(x: Int) extends Expr
-  case class Prod(e1: Expr, e2: Expr) extends Expr
+  object Expr:
+    case class Number(n: Int) extends Expr
+    case class Sum(e1: Expr, e2: Expr) extends Expr
+    case class Var(x: Int) extends Expr
+    case class Prod(e1: Expr, e2: Expr) extends Expr
 
   def eval(e: Expr): Int = e match
-     case Numberr(n) => n
-     case Sum(e1, e2) => eval(e1) + eval(e2)
-     case Prod(e1, e2) => eval(e1) * eval(e2)
+     case Expr.Number(n) => n
+     case Expr.Sum(e1, e2) => eval(e1) + eval(e2)
+     case Expr.Prod(e1, e2) => eval(e1) * eval(e2)
 
   def show(e: Expr): String = e match
-    case Numberr(n) => n.toString
-    case Sum(e1, e2) => s"${show(e1)} + ${show(e2)}"
-    case Prod(e1, e2) => e1 match
-      case Sum(e2, e3) => s"(${show(e1)}) * ${show(e2)}"
+    case Expr.Number(n) => n.toString
+    case Expr.Sum(e1, e2) => s"${show(e1)} + ${show(e2)}"
+    case Expr.Prod(e1, e2) => e1 match
+      case Expr.Sum(e2, e3) => s"(${show(e1)}) * ${show(e2)}"
       case _ => s"${show(e1)} * ${show(e2)}"
 
 
 
   def main(args: Array[String]): Unit = {
-    val x = pme.Numberr(3)
-    val y = pme.Numberr(7)
-    val z = pme.Sum(x, y)
-    println(pme.eval(pme.Sum(x, y)))
-    println(show(pme.Prod(pme.Sum(pme.Numberr(2),pme.Numberr(6)),z)))
-    println(show(pme.Prod(pme.Prod(pme.Numberr(2),pme.Numberr(6)),z)))
+    val x = pme.Expr.Number(3)
+    val y = pme.Expr.Number(7)
+    val z = pme.Expr.Sum(x, y)
+    println(pme.eval(pme.Expr.Sum(x, y)))
+    println(pme.show(pme.Expr.Prod(pme.Expr.Sum(pme.Expr.Number(2),pme.Expr.Number(6)),z)))
+    println(pme.show(pme.Expr.Prod(pme.Expr.Prod(pme.Expr.Number(2),pme.Expr.Number(6)),z)))
     }
 
 }
